40b25bac84a4426aff6453872e8be7ee282acd10
erika cueva <ecueva88@gmail.com>
2021-03-22 17:34:33 -0700

    fixed date format

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ var APIkey = "9aef3a1366ac2f774ab80c44e3b4e8cd";
 
 // check for existing cities in local storage
 function find(c) {
-  for (var i = 0; i < sCity.length; i++) {
-    if (c.toUpperCase() === sCity[i]) {
+  for (var i = 0; i < cityArray.length; i++) {
+    if (c.toUpperCase() === cityArray[i]) {
       return -1;
     }
   }
@@ -47,7 +47,8 @@ function currentWeather(city) {
     var weatherIcon = response.weather[0].icon;
     var iconURL =
       "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
-    var date = new Date(response.dtx1000).toLocaleDateString();
+    var date = new Date(response.dt*1000).toDateString();
+  
     // concatinate the date and icon
     $(currentCity).html(
       response.name + "(" + date + ")" + "<img src=" + iconURL + ">"
@@ -62,20 +63,20 @@ function currentWeather(city) {
     UV(response.coord.lon, response.coord.lat);
     fiveDayForecast(response.id);
     if (response.cod == 200) {
-      cityArray.JSON.parse(localStorage.getItem("cityname"));
+      cityArray=JSON.parse(localStorage.getItem("cityname"));
       console.log(cityArray);
       if (cityArray == null) {
         cityArray = [];
         cityArray.push(city.toUpperCase()
         );
         localStorage.setItem("cityname", JSON.stringify(cityArray));
-        pushtocities(city);
+        find(city);
       }
       else {
         if(find(city)>0) {
           cityArray.push(city.toUpperCase());
           localStorage.setItem("cityname",JSON.stringify(cityArray));
-          addToList(city);
+          find(city);
         }
       }
     }
@@ -133,7 +134,7 @@ function loadCities(){
   if(cityArray!==null){
       cityArray=JSON.parse(localStorage.getItem("cityname"));
       for(i=0; i<cityArray.length;i++){
-          addToList(cityArray[i]);
+          find(cityArray[i]);
       }
       city=cityArray[i-1];
       currentWeather(city);
